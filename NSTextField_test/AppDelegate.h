5056193242445bd59ae5b7fe953292d0da13f198
Gregory Casamento <greg.casamento@gmail.com>
2020-08-31 01:01:14 -0400

    Initial Commit

---
+++ NSTextField_test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSTextField_test
+//
+//  Created by Gregory Casamento on 8/31/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSTextField_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSTextField_test
+//
+//  Created by Gregory Casamento on 8/31/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
